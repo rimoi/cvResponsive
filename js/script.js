7d755f91ae0d8b3f412fbc4e85854852bca893b9
sidikhalifa <sidi.khalifa@live.fr>
2016-07-15 01:39:36 +0200

    creation de la parti entete et la parti competence

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// document ici correpond au notre fichier index.html si elle est prete(ready) exécute moi alors ce code
+$(function(){
+
+    $('#cacher').click(function(){
+       $('[data-toggle="tooltip"]').tooltip();
+    });
+       
+});
+    
+    
