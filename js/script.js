b8f35b7f57a4005f7f438a6650a28372ebdfb388
sidikhalifa <sidi.khalifa@live.fr>
2016-07-16 15:22:24 +0200

    CV Final

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // document ici correpond au notre fichier index.html si elle est prete(ready) exécute moi alors ce code
 $(function(){
-
+    /* Animation au moment de défilage d'un block vers en autre */
     $('.navbar a, footer a').click(function(event){
       event.preventDefault();
       var hash = this.hash;
